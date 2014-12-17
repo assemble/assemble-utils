1948867ee37692c3b7978cab377b3fc706e9f786
Brian Woodward <brian.woodward@gmail.com>
2014-12-16 21:39:01 -0500

    new project files

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * assemble-utils <https://github.com/assemble/assemble-utils>
+ *
+ * Copyright (c) 2014 Brian Woodward, contributors.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
